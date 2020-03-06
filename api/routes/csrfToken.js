44447102b350cb255b68ee88c37387ca255bc942
Ferhnir <zax1984@gmail.com>
2020-03-06 10:19:40 +0000

    Added 'dotenv' package and updated security for JWT

--- api/routes/csrfToken.js
+++ api/routes/csrfToken.js
@@ -5,14 +5,14 @@ const checkIfLocal = require('../middlewares/validateLocalReq');
 
 const jwt = require('jsonwebtoken');
 var token = jwt.sign({ 
-    // exp: Math.floor(Date.now() / 1000) + (60 * 60),
-    data: 'myprivkey'
+    exp: Math.floor(Date.now() / 1000) + (60 * 60),
+    data: process.env.JWT_SECRET_KEY
  }, 'shhhhhh');
 
 
 router.get('/tokenrequest', checkIfLocal, (req, res) => {
     res.status(200).json({
-        message: token
+        jwt_token: token
     });
 });
 
