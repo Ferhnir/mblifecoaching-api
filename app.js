dd72b8ade5aca2192b5df8005e8f31c7810d481a
Ferhnir <zax1984@gmail.com>
2020-03-09 13:58:16 +0000

    Update for host access

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const tokenRoute = require('./api/routes/jwtToken');
 const contactMeBox = require('./api/routes/messages/contactMe');
 
 const corsOptions = {
-    origin: ['https://localhost:3010', 'https://mzdunski.website/'],
+    origin: ['https://localhost:3010', 'https://mzdunski.website/','http://178.128.35.52:3010/', 'https://mzdunski.website/#'],
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
     methods: 'PUT, POST, DELETE, GET, PATCH',
     allowedHeaders: ["Origin","X-Requested-With","Content-Type","Accept","Authorization","X-Access-Token"]
