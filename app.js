7102ce4d3db0684b7c3c3ff23412390effd47bc6
Ferhnir <zax1984@gmail.com>
2020-03-10 11:26:25 +0000

    Minor changes in app.js

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ const tokenRoute = require('./api/routes/jwtToken');
 const contactMeBox = require('./api/routes/messages/contactMe');
 
 const corsOptions = {
-    origin: ['https://localhost:3010', 'https://mzdunski.website', 'http://mzdunski.website'],
+    origin: [
+      'https://localhost:3010', 
+      'https://mzdunski.website', 
+      'https://mbfitlifecoaching.fitness',
+      'http://mzdunski.website', 
+      'http://mbfitlifecoaching.fitness'
+    ],
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
     methods: 'PUT, POST, DELETE, GET, PATCH',
     allowedHeaders: ["Origin","X-Requested-With","Content-Type","Accept","Authorization","X-Access-Token"]
